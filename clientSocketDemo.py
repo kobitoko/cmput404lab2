db1971a67e9d10957b655f9fda5e59bf037915b9
kobitoko <kobitoko@gmail.com>
2016-09-15 14:21:23 -0600

    basic hardcoded curl example

---
+++ clientSocketDemo.py
@@ -0,0 +1,20 @@
+import socket
+
+# Using Sockets from the OS to make clients.
+# socket.AF_INET means use this socket to communicate to the internet
+# socket.SOCK_STREAM means we want to use TCP!
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# this function tries to do C, hence 1 tuple as argument.
+clientSocket.connect(("www.google.com",80))
+
+clientSocket.sendall("GET / HTTP/1.0\r\n\r\n")
+
+# Loop to receive things:
+while(1):
+    # Ask for 1024bytes at a time.
+    part = clientSocket.recv(1024)
+    if(len(part) > 0):
+        print part
+    else: # part will be "" when the connection is done and we can exit.
+        exit(0)
+
